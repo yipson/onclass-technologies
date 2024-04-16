f6af8de44fe3316dcfd4e14d82a8f1f5054956f6
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2024-04-16 01:19:34 -0500

    initial commit

---
+++ src/main/java/com/pragma/onclass/technologies/OnclassTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.onclass.technologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnclassTechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnclassTechnologiesApplication.class, args);
+	}
+
+}
