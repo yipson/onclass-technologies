7f42ecced7568ecbaebed57901b22ccbe51056d5
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2024-04-29 12:47:35 -0500

    ONCLASS-HU-1: Adding fields to mapper

--- src/main/java/com/pragma/onclass/technologies/adapter/mapper/TechnologyMapper.java
+++ src/main/java/com/pragma/onclass/technologies/adapter/mapper/TechnologyMapper.java
@@ -1,16 +1,24 @@
-package com.pragma.onclass.technologies.adapter.driving.rest.mapper;
+package com.pragma.onclass.technologies.adapter.mapper;
 
+import com.pragma.onclass.technologies.adapter.driven.persistence.entity.TechnologyEntity;
 import com.pragma.onclass.technologies.adapter.driving.rest.model.TechnologyDto;
 import com.pragma.onclass.technologies.adapter.driving.rest.model.request.TechnologyRq;
 import com.pragma.onclass.technologies.domain.model.Technology;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
 public interface TechnologyMapper {
     TechnologyMapper INSTANCE = Mappers.getMapper(TechnologyMapper.class);
 
     TechnologyDto domainToDto(Technology domain);
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "description", target = "description")
     Technology rqToDomain(TechnologyRq request);
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "description", target = "description")
+    TechnologyEntity domainToEntity(Technology domain);
 }
