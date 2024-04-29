18de1bbd8c79080f4c2d526d1b26afbd6628b422
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2024-04-29 12:43:13 -0500

    ONCLASS-HU-1: Adding R2dbc configuration

--- src/main/java/com/pragma/onclass/technologies/OnclassTechnologiesApplication.java
+++ src/main/java/com/pragma/onclass/technologies/OnclassTechnologiesApplication.java
@@ -2,7 +2,9 @@ package com.pragma.onclass.technologies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+@EnableR2dbcRepositories
 @SpringBootApplication
 public class OnclassTechnologiesApplication {
 
