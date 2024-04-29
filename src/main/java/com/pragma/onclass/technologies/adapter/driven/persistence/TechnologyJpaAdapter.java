aa3d2a2ecc32f119e4919ac7aa90874daad7dd45
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2024-04-29 12:39:58 -0500

    ONCLASS-HU-1: Adding JpaAdapter and its Repostiroy together with its Entity

--- src/main/java/com/pragma/onclass/technologies/adapter/driven/persistence/TechnologyJpaAdapter.java
+++ src/main/java/com/pragma/onclass/technologies/adapter/driven/persistence/TechnologyJpaAdapter.java
@@ -1,16 +1,36 @@
 package com.pragma.onclass.technologies.adapter.driven.persistence;
 
+import com.pragma.onclass.technologies.adapter.driven.persistence.entity.TechnologyEntity;
+import com.pragma.onclass.technologies.adapter.mapper.TechnologyMapper;
 import com.pragma.onclass.technologies.domain.model.Technology;
 import com.pragma.onclass.technologies.domain.repository.TechnologyRepositoryPort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class TechnologyJpaAdapter implements TechnologyRepositoryPort {
 
-    private static TechnologyRepository technologyRepository;
-    @Override
-    public Technology save(Technology technology) {
+    private final TechnologyRepository technologyRepository;
+    private final TechnologyMapper technologyMapper;
 
-        return null;
+    @Autowired
+    public TechnologyJpaAdapter(TechnologyRepository technologyRepository, TechnologyMapper technologyMapper) {
+        this.technologyRepository = technologyRepository;
+        this.technologyMapper = technologyMapper;
+    }
+
+
+    @Override
+    public Mono<Technology> save(Technology technology) {
+        TechnologyEntity technologyEntityToSave = technologyMapper.domainToEntity(technology);
+        Mono<TechnologyEntity> technologyEntitySaved = technologyRepository.save(technologyEntityToSave);
+        return technologyEntitySaved.map(technologyEntity ->
+             Technology.builder()
+                    .id(technologyEntity.getId())
+                    .name(technologyEntity.getName())
+                    .description(technologyEntity.getDescription())
+                    .build()
+        );
     }
 }
--- src/main/java/com/pragma/onclass/technologies/adapter/driven/persistence/TechnologyRepository.java
+++ src/main/java/com/pragma/onclass/technologies/adapter/driven/persistence/TechnologyRepository.java
@@ -1,4 +1,10 @@
 package com.pragma.onclass.technologies.adapter.driven.persistence;
 
-public interface TechnologyRepository {
+import com.pragma.onclass.technologies.adapter.driven.persistence.entity.TechnologyEntity;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TechnologyRepository extends R2dbcRepository<TechnologyEntity, Long> {
+
 }
